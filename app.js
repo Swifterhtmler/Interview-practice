fa7b834b80bd7eed1fee4c1a9fd43d98a4bedabd
Riku Kuisma <rikukuisma@icloud.com>
2024-07-11 01:55:05 +0300

    ...

--- app.js
+++ app.js
@@ -15,12 +15,45 @@
 //             console.log(reverse("CodingMoney"))
 
 
-function reverse(str) {
+// function reverse(str) {
     
-    let reversed = str.split('').reverse().join('')
+//     let reversed = str.split('').reverse().join('')
     
-    return reversed
+//     return reversed
     
+// }
+    
+//     console.log(reverse("CodingMoney"))
+
+// function reverseInt(n) {
+//     reversed =  n.toString().split('').reverse().join('')
+    
+
+//     return parseInt(reversed) * Math.sign(n)
+// }
+
+// console.log(reverseInt(-15))
+
+
+function palindrome(str) {
+  
+    let reversed = str.split('') // .reverse().join('')
+
+    console.log(reversed)
+
+    for (let i = 0; i < reversed.length; i++) {
+         if (reversed[i] === " ") {
+            reversed.pop[" "]
+         }
+    }
+    console.log(reversed)
+
+//   if (reversed === str) {  
+//      return true
+//    } else {
+//     return false
+//    }
+
 }
-    
-    console.log(reverse("CodingMoney"))
+
+console.log(palindrome("race car"))
