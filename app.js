3f80fc2cae63e903d820050dfcc1c61c89da1afa
Riku Kuisma <rikukuisma@icloud.com>
2024-07-10 21:55:58 +0300

    created second branch

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 
 
-function reverse(str) {
-        
-        let reversed = ""
-
-        for (let i = 0; i < str.length; i++) {
-            reversed =  str[i] + reversed
-            }
-
-            return reversed
-
-            }
-
-            console.log(reverse("CodingMoney"))
-
-
 // function reverse(str) {
+        
+//         let reversed = ""
+
+//         for (let i = 0; i < str.length; i++) {
+//             reversed =  str[i] + reversed
+//             }
+
+//             return reversed
+
+//             }
+
+//             console.log(reverse("CodingMoney"))
+
+
+function reverse(str) {
     
-//     let reversed = str.split('').reverse().join('')
+    let reversed = str.split('').reverse().join('')
     
-//     return reversed
+    return reversed
     
-// }
+}
     
-//     console.log(reverse("CodingMoney"))
+    console.log(reverse("CodingMoney"))
