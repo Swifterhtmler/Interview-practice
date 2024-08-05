a7cf6ee99d9608f18fc2ceb1d1f42014f98bb057
Riku Kuisma <130903460+Swifterhtmler@users.noreply.github.com>
2024-08-06 00:55:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -23,4 +23,26 @@ function reverse(str) {
     
 }
     
-    console.log(reverse("CodingMoney"))
+    console.log(reverse("Hello"));
+
+
+const expect = function(val) {
+    return {
+        toBe: function(sval) {
+            if (val === sval) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(tval) {
+            if (val !== tval) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
+};
+
+console.log(expect(5).toBe(5))
